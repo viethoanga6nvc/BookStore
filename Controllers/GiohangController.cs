e62e033c6d93dc933f0ded450c467bf771085cc1
viethoanga6nvc <76078772+viethoanga6nvc@users.noreply.github.com>
2022-07-02 13:54:29 +0700

    Add files via upload

---
+++ Controllers/GiohangController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class GiohangController : Controller
+    {
+        QLBanSachDB db = new QLBanSachDB();
+        // GET: Giohang
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang==null)
+            {
+                lstGiohang = new List<Giohang>();
+                Session["Giohang"] = lstGiohang;
+            }
+            return lstGiohang;
+        }
+        public ActionResult ThemGiohang(int iMasach, string strURL)
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.Find(n => n.iMasach == iMasach);
+            if (sanpham==null)
+            {
+                sanpham = new Giohang(iMasach);
+                lstGiohang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURL);
+            }
+        }
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang!=null)
+            {
+                iTongSoLuong = lstGiohang.Sum(n => n.iSoluong);
+            }
+            return iTongSoLuong;
+        }
+
+        private double Tongtien()
+        {
+            double iTongtien = 0;
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang!=null)
+            {
+                iTongtien = lstGiohang.Sum(n => n.dThanhtien);
+            }
+            return iTongtien;
+        }
+
+        public ActionResult Giohang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            if (lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "BookStore");
+            }
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = Tongtien();
+            return View(lstGiohang);
+        }
+    }
+}
---
+++ Models/Giohang.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BookStore.Models;
+
+namespace BookStore.Models
+{
+    public class Giohang
+    {
+        QLBanSachDB db = new QLBanSachDB();
+        public int iMasach { get; set; }
+        public string sTenSach { get; set; }
+        public string sAnhbia { get; set; }
+        public Double dDongia { get; set; }
+        public int iSoluong  { get; set; }
+        public Double dThanhtien 
+        {
+            get { return iSoluong * dDongia; }
+        }
+        public Giohang(int Masach)
+        {
+            iMasach = Masach;
+            SACH sach = db.SACHes.Single(n => n.Masach == iMasach);
+            sTenSach = sach.Tensach;
+            sAnhbia = sach.Anhbia;
+            dDongia = double.Parse(sach.Giaban.ToString());
+            iSoluong = 1;
+        }
+    }
+}
